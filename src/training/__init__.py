298eb7b3f4fe46bb8350cc88e70219c65fb761cf
Bao Nguyen <nguyenducbao2005@gmail.com>
2026-07-29 20:51:43 +0700

    Expose training utilities

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,6 +1,7 @@
 from .engine import train_one_epoch, train_one_epoch_kd, evaluate, predict
 from .kd_trainer import KDTrainer
 from .trainer import Trainer
+from .optim import build_optimizer, build_scheduler
 
 __all__ = [
     "train_one_epoch",
@@ -9,4 +10,6 @@ __all__ = [
     "predict",
     "Trainer",
     "KDTrainer",
+    "build_optimizer",
+    "build_scheduler",
 ]
